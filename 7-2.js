97a3d5eae57b529cadac8c9131083604ba0dbc1f
dylansteen <dsteen@petalmd.com>
2021-12-07 13:23:27 -0500

    7-2

---
+++ 7-2.js
@@ -0,0 +1,29 @@
+const util = require('util');
+const fs = require('fs');
+const readFile = util.promisify(fs.readFile);
+
+function getData() { 
+  return readFile('input.txt', 'utf8').then((data) => data.split(',').filter(num => !!num).map(stringNum => +stringNum));
+}
+
+const triangle = (n) => (n*(n+1))/2
+
+async function main() {
+  const positions = await getData();
+  const furthestCrab = Math.max(...positions);
+  let minFuel = Infinity;
+  let currentPos = 0;
+
+  while (currentPos <= furthestCrab) {
+    const fuelForPosition = positions.slice(0).reduce((totalFuel, currentCrab, _, arr) => { 
+      const newTotal = totalFuel + triangle(Math.abs(currentCrab - currentPos));
+      return newTotal;
+    }, 0);
+    minFuel = Math.min(minFuel, fuelForPosition);
+    currentPos++
+  }
+
+  console.log(minFuel);
+}
+
+main();
