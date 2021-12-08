cc33586bdd84923c4a495bad27e890a43bdc0ec5
dylansteen <dsteen@petalmd.com>
2021-12-07 20:19:43 -0500

    7-2 fast but ugly version

---
+++ 7-2-fast.js
@@ -0,0 +1,46 @@
+const util = require('util');
+const fs = require('fs');
+const readFile = util.promisify(fs.readFile);
+const {performance} = require('perf_hooks');
+
+
+function getData() { 
+  return readFile('input.txt', 'utf8').then((data) => data.split(',').filter(num => !!num).map(stringNum => +stringNum));
+}
+
+const triangle = (n) => (n*(n+1))/2
+
+const triangles = {};
+
+const memoizedTriangle = (n) => {
+  const nThTriangle = triangles[n];
+  if (nThTriangle) {
+    return nThTriangle;
+  } else {
+    const triangleVal = triangle(n);
+    triangles[n] = triangleVal;
+    return triangleVal;
+  }
+}
+
+async function main() {
+  const positions = await getData();
+  const furthestCrab = Math.max(...positions);
+  let minFuel = Infinity;
+  let currentPos = 0;
+
+  while (currentPos <= furthestCrab) {
+    let i = 0;
+    let fuelForPosition = 0;
+    while(i < positions.length - 1) {
+      fuelForPosition += triangle(Math.abs(positions[i] - currentPos));
+      if(fuelForPosition > minFuel) break;
+      i++;
+    }
+    minFuel = Math.min(minFuel, fuelForPosition);
+    currentPos++
+  }
+  console.log(minFuel);
+}
+
+main();
